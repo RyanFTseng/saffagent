9ad4cb2c6389f2ec1bacb2391e343bf5a299b506
ryanftseng <rtseng99@yahoo.com>
2025-06-20 12:30:54 -0700

    remove solo agent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from dotenv import load_dotenv
 from pydantic import BaseModel , Field
-from langchain_nvidia import ChatNVIDIA
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import PydanticOutputParser
 from langchain.agents import create_tool_calling_agent, AgentExecutor
@@ -322,49 +321,4 @@ def run_chatbot():
 
 if __name__ == "__main__":
     run_chatbot()
-'''
-
-'''
-prompt = ChatPromptTemplate.from_messages(
-    [
-        (
-            "system",
-            """
-            You are a pro gamer with multiple championship titles.
-            Answer the user query and use neccessary tools. 
-            Wrap the output in this format and provide no other text\n{format_instructions}
-            """,
-        ),
-        ("placeholder", "{chat_history}"),
-        ("human", "{query}"),
-        ("placeholder", "{agent_scratchpad}"),
-    ]
-).partial(format_instructions=parser.get_format_instructions())
-
-
-#init agent and tools
-#tools = []
-#tools = [search_tool, wiki_tool, save_tool]
-tools = [search_tool]
-
-agent = create_tool_calling_agent(
-    llm=llm,
-    prompt=prompt,
-    tools=tools
-)
-
-agent_executor = AgentExecutor(agent = agent, tools = tools, verbose = True)
-query = input("What can I help you with? ")
-raw_response = agent_executor.invoke({"query":query}) #match with human inputs in prompt def
-print(raw_response)
-
-#parse raw output
-try:
-    structured_response = parser.parse(raw_response.get("output"))
-except Exception as e:
-    print("error parsing response", e, " Raw Respone - ", raw_response)
-
-
-
-#print(structured_response)
 '''
