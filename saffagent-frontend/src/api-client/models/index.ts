85f589fd91157bb5c20d54b79f20f33f679c28ed
ryanftseng <rtseng99@yahoo.com>
2025-06-26 12:49:57 -0700

    typescript client

---
+++ saffagent-frontend/src/api-client/models/index.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './ChatRequest';
+export * from './HTTPValidationError';
+export * from './ValidationError';
+export * from './ValidationErrorLocInner';
